ba8ad849999f296525d1ad0daf7687a03a4e7b2f
casoul <lijingwei9060@qq.com>
2024-05-17 13:36:17 +0800

    mod skb

--- README.md
+++ README.md
@@ -24,3 +24,13 @@ cargo build
 ```bash
 RUST_LOG=info cargo xtask run
 ```
+
+## demo
+
+```shell
+sudo ip link add vpc-25932F01 type veth peer name vpc-25932F01-1
+sudo ifconfig vpc-25932F01 hw ether fe:00:25:93:2F:01 up
+sudo ifconfig vpc-25932F01-1 hw ether d0:00:25:93:2F:01 up
+
+sudo tcpdump -e -ni vpc-25932F01-1 -vvv
+```
--- fog-net-common/src/ctx/skb.rs
+++ fog-net-common/src/ctx/skb.rs
@@ -1,4 +1,50 @@
+use core::mem;
+
 use aya_ebpf::programs::TcContext;
+use networktype::{eth::EthHdr, ip::{Ipv4Hdr, Ipv6Hdr}, EtherType};
+
+
+#[inline(always)]
+pub fn ptr_at<T>(ctx: &TcContext, offset: usize) -> Option<*const T> {
+    let start = ctx.data();
+    let end = ctx.data_end();
+    let len = mem::size_of::<T>();
+
+    if start + offset + len > end {
+        return None;
+    }
+
+    Some((start + offset) as *const T)
+}
+
+#[inline(always)]
+pub fn ptr_at_mut<T>(ctx: &TcContext, offset: usize) -> Option<*mut T> {
+    let ptr = ptr_at::<T>(ctx, offset)?;
+    Some(ptr as *mut T)
+}
+
+#[inline(always)]
+pub fn parse_l2_header(ctx: &TcContext) -> Option<EtherType> {
+    ptr_at::<EthHdr>(ctx, 0).map(|e| unsafe { (*e).ether_type })
+}
+
+#[inline(always)]
+pub fn parse_ipv4_header(ctx: &TcContext) -> Option<&Ipv4Hdr> {
+    let ether_type = parse_l2_header(ctx)?;
+    match ether_type {
+        EtherType::Ipv4 => ptr_at::<Ipv4Hdr>(ctx, EthHdr::LEN).map(|e| unsafe { &(*e) }),
+        _ => None,
+    }
+}
+
+#[inline(always)]
+pub fn parse_ipv6_header(ctx: &TcContext) -> Option<&Ipv6Hdr> {
+    let ether_type = parse_l2_header(ctx)?;
+    match ether_type {
+        EtherType::Ipv6 => ptr_at::<Ipv6Hdr>(ctx, EthHdr::LEN).map(|e| unsafe { &(*e) }),
+        _ => None,
+    }
+}
 
 /// get metadata
 pub fn ctx_load_meta(ctx: &TcContext, offset: u32) -> u32{
